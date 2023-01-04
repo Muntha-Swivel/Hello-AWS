5c7a91ff07bac1b2e19c9298984fcc7034a13f4c
Muntha-Swivel <munthasir@swivelgroup.com.au>
2023-01-04 10:53:47 +0530

    changed port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const port = process.env.port || 5000;
+const port = 5000;
 app.get("/", function (req, res) {
   res.send("Hello World");
 });
